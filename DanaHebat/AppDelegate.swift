74f528a1d2748f113f0888c291950b0611aa4b3a
hekang1992 <hekang19921111@hotmail.com>
2026-01-09 10:13:25 +0800

    first commit

---
+++ DanaHebat/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  DanaHebat
+//
+//  Created by hekang on 2026/1/9.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let viewController = LaunchViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Pods/Target Support Files/Pods-DanaHebat/Pods-DanaHebat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DanaHebat : NSObject
+@end
+@implementation PodsDummy_Pods_DanaHebat
+@end
