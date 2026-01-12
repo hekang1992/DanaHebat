87d0df4cc8fc0a91fc24c9e28166c7b9832b0a79
hekang1992 <hekang19921111@hotmail.com>
2026-01-12 18:06:53 +0800

    fix:guss=101

--- DanaHebat/Other/LocationInfo.swift
+++ DanaHebat/Other/LocationInfo.swift
@@ -123,7 +123,7 @@ extension SimpleLocationManager {
             
             alert.addAction(UIAlertAction(title: LanguageManager.localizedString(for: "Cancel"), style: .cancel))
             
-            alert.addAction(UIAlertAction(title: LanguageManager.localizedString(for: "Go to  settings"), style: .default) { _ in
+            alert.addAction(UIAlertAction(title: LanguageManager.localizedString(for: "Go to settings"), style: .default) { _ in
                 guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
                 UIApplication.shared.open(url)
             })
